fa2e625f737d59b3bea350ce98fd30d072a2611a
paulaami <pmilkowska@onet.pl>
2022-02-20 17:30:48 +0100

    add new files

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+const burgerBtn = document.querySelector(".navigation__container-hamburger");
+const icon = burgerBtn.querySelector(".icon");
+const mediaQuery = window.matchMedia("(min-width: 1439px)");
+
+const handleNav = () => {
+  const nav = document.querySelector(".list-level-one");
+  nav.classList.toggle("active");
+  if (nav.classList.contains("active")) {
+    icon.src = `./assets/svg/close.svg`;
+    burgerBtn.setAttribute("aria-expanded", "true");
+  } else {
+    icon.src = `./assets/svg/hamburger.svg`;
+    burgerBtn.setAttribute("aria-expanded", "false");
+  }
+};
+
+const changeIcon = () => {
+ 
+  if (mediaQuery.matches) {
+    icon.src = `./assets/svg/account.svg`;
+    burgerBtn.disabled = true;
+    console.log(allImg)
+  } else {
+    icon.src = `./assets/svg/hamburger.svg`;
+    burgerBtn.disabled = false;
+  }
+};
+
+
+mediaQuery.addListener(changeIcon);
+burgerBtn.addEventListener("click", handleNav);
