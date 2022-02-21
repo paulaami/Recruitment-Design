f06b64e61f70a85ada969b3149f4ea9e6ec373e1
paulaami <pmilkowska@onet.pl>
2022-02-21 15:06:04 +0100

    updated js code and layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 const burgerBtn = document.querySelector(".navigation__container-hamburger");
 const burgerIcon = burgerBtn.querySelector(".burger-icon");
 const closeIcon = burgerBtn.querySelector(".close-icon");
-const mediaQuery = window.matchMedia("(min-width: 1439px)");
+let pathname = window.location.pathname;
 
 const handleNav = () => {
-  const nav = document.querySelector(".list-level-one");
+  const nav = document.querySelector(".navigation__container-list");
   nav.classList.toggle("active");
   if (nav.classList.contains("active")) {
     burgerIcon.style.display = "none";
@@ -19,46 +19,22 @@ const handleNav = () => {
 
 burgerBtn.addEventListener("click", handleNav);
 
-const switchIcon = () => {
-  if (mediaQuery.matches) {
-    burgerBtn.disabled = true;
-  } else {
-    burgerBtn.disabled = false;
-  }
-};
-
-mediaQuery.addListener(switchIcon);
-
-const currentPage = document.querySelector(".header__nav-current");
-const currentSubpage = document.querySelector(".header__nav-subpage");
-const aboutLink = document.querySelector(".about-link");
-const collectionLink = document.querySelector(".collection-link");
-let pathname = window.location.pathname;
-
 const detectPage = () => {
+  const currentPage = document.querySelector(".header__nav-current");
+  const currentSubpage = document.querySelector(".header__nav-subpage");
+  const aboutLink = document.querySelector(".about-link");
+  const collectionLink = document.querySelector(".collection-link");
   if (pathname.includes("about")) {
     currentPage.textContent = "About";
     aboutLink.style.color = "#000";
   } else if (pathname.includes("collection") || pathname.includes("item")) {
     currentPage.textContent = "Collection";
     collectionLink.style.color = "#fff";
-    if (currentSubpage) currentSubpage.textContent = 'Collection Item'
+    if (currentSubpage) currentSubpage.textContent = "Collection Item";
   }
 };
 
 detectPage();
 
-// switch (pathname) {
-//   case "/about.html":
-//   case "/Recruitment-Design/about.html": //path contains about
-//     currentPage.textContent = "About";
-//     aboutLink.style.color = "#000";
-//     break;
-//   case "/collection.html":
-//   case "/Recruitment-Design/collection.html": //path contains collection
-//     currentPage.textContent = "Collection";
-//     collectionLink.style.color = "#fff";
-//     break;
-// }
 
 console.log(pathname);
